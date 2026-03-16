9deb49374b6a7881ff887c778726d78167b7eb9f
Clara <clarapujoldecarvalho@gmail.com>
2026-03-15 22:47:18 -0300

    Atualizar arquivos

--- src/ConversaoTemperatura.java
+++ src/ConversaoTemperatura.java
@@ -1,27 +1,34 @@
 import java.util.Scanner;
 
 public class ConversaoTemperatura {
-    public static void main(String[] args) {
 
+    public static String pegarInput (String texto){
         Scanner leitor = new Scanner(System.in);
+        System.out.print(texto);
 
-        System.out.print("Digite uma temperatura em Celsius: ");
-        float tempCelsius = leitor.nextFloat();
+        return leitor.nextLine();
+    }
 
-        System.out.println("Escolha a conversão:");
-        System.out.println("1- Fahrenheit");
-        System.out.println("2- Kelvin");
+    public static int pegarInputInt (String texto){
+        return Integer.parseInt( pegarInput(texto) );
+    }
 
-        int conversao = leitor.nextInt();
+    public static float pegarInputFloat (String texto){
+        return Float.parseFloat( pegarInput(texto) );
+    }
+
+    public static void main(String[] args) {
+        float tempCelsius = pegarInputFloat("Digite uma temperatura em Celsius: ");
+        int conversao = pegarInputInt("Escolha a conversão:  1- Fahrenheit  2- Kelvin:");
 
         switch (conversao) {
             case 1:
-                float tempFahrenheit = (tempCelsius * 9 / 5) + 32;
+               float tempFahrenheit = (tempCelsius * 9 / 5) + 32;
                 System.out.println("A temperatura em Fahrenheit é: " + tempFahrenheit);
             case 2:
                 float tempKelvin = (tempCelsius + 273);
-                System.out.println("A temperatura em Kelvin é: " + tempKelvin);
-        }
+               System.out.println("A temperatura em Kelvin é: " + tempKelvin);
 
+       }
     }
 }
