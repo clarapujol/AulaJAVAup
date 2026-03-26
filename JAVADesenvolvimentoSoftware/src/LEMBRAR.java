ca33abea724a41ae8eb321c099320f8f23ea77c4
Clara Pujol <seu-email@example.com>
2026-03-26 16:01:10 -0300

    Ajuda para Commit

--- JAVADesenvolvimentoSoftware/src/LEMBRAR.java
+++ JAVADesenvolvimentoSoftware/src/LEMBRAR.java
@@ -1,3 +1,7 @@
 // SEMPRE QUE CRIAR UM ARQUIVO, DAR O COMMIT NO GITHUB
 // PARA NÃO PERDER O CÓDIGO
 
+// # Rotina normal de commit
+// git add .
+// git commit -m "Descrição do que foi feito"
+// git push
